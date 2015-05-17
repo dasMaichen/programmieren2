77e3d1e2e9e214f3574b670738878646d9706c7a
dasMaichen <m@ichen.nix>
2015-05-17 11:52:29 +0200

    Monster bekommt nun random Item und Gold.

--- src/Monster.java
+++ src/Monster.java
@@ -1,7 +1,16 @@
+import java.util.Random;
+
 /**
  * The type Monster.
  */
 public class Monster extends Creature {
+
+
+    /**
+     * The Name.
+     */
+    private String name;
+
 
     /**
      * Gets name.
@@ -11,11 +20,6 @@ public class Monster extends Creature {
     public String getName() {
         return name;
     }
-
-    /**
-     * The Name.
-     */
-    private String name;
 
     /**
      * Instantiates a new Monster.
@@ -42,10 +46,24 @@ public class Monster extends Creature {
      * @param hp        the hp
      * @param atk       the atk
      * @param hitChance the hit chance
+     *
+     * Monster bekommt jetzt random Item und random Gold.
+     *
      */
     public Monster(String name, int hp, int atk, double hitChance) {
         super(hp, atk, hitChance);
         this.name = name;
+
+        Random generator = new Random();
+        this.gold = generator.nextInt(101);
+
+        int anzahlZufaelligerItems = generator.nextInt(5);
+        for (int i = 0; i < anzahlZufaelligerItems; i++) {
+            Item item = new Item();
+            inventar.add(item);
+        }
+
+
     }
 
     /**
