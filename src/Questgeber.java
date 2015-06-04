349e65863cc38b2558689ccf579b7d343a859565
dasMaichen <m@ichen.nix>
2015-06-04 09:43:45 +0200

    Klasse Questgeber erstellt.

---
+++ src/Questgeber.java
@@ -0,0 +1,24 @@
+/**
+ * Created by mai on 03.06.15.
+ */
+public class Questgeber {
+
+    public Questlog questlog = new Questlog();
+
+
+    public ListAufg<Quest> getQuestl(){
+        return questlog;
+    }
+
+
+
+    public boolean istQuestAbgeschlossen(Quest quest){
+
+
+
+        return false;
+
+    }
+
+
+}
